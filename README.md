8baf9f44b361ffc7312f4af903580972cdb22e63
vdigmJiMin <andy@vdigm.com>
2024-09-03 18:45:34 +0900

    초기 커밋 메시지입니다.

---
+++ README.md
@@ -0,0 +1,3 @@
+# mlCapture
+
+Developed with Unreal Engine 5
---
+++ Source/mlCapture/mlCapture.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "mlCapture.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, mlCapture, "mlCapture" );
